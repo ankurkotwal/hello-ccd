72a42149bf5211190c958b511b87dbb90bd7608d
Ankur Kotwal <kotwal@google.com>
2019-12-04 22:36:56 +0530

    Added run.sh

---
+++ run.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+pack build gcr.io/ankur-cloud-run/hello-ccd --path . --builder heroku/buildpacks
+docker push gcr.io/ankur-cloud-run/hello-ccd
+gcloud run deploy hello-ccd\
+          --project=ankur-cloud-run\
+          --platform=managed\
+          --region=asia-northeast1\
+          --image=gcr.io/ankur-cloud-run/hello-ccd\
+          --memory=512Mi\
+          --allow-unauthenticated
